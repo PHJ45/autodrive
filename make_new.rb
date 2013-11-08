2089a0d0e993d0b46ad91e2b5df0a7b873089b83
github username <github email address>
2013-11-07 21:21:05 -0500

    .

--- make_new.rb
+++ make_new.rb
@@ -1,10 +1,9 @@
-puts "Github User Name?"
+puts "Please enter your Github user name?"
 gitname = gets.strip 
 
 system("curl -s -u#{gitname} https://api.github.com/user/repos -d '{\"name\":\"autodrive_profile\"}'")
 Dir.chdir `echo ~`.chomp
 system("mkdir autodrive")
-system("pwd")
 system("cp .bash_profile autodrive/autodrive_profile.txt")
 Dir.chdir `echo autodrive`.chomp
 system("git init")
